17c6a968a3267d5c890b8f7857880ef4c4388f17
ben <sobenkh35@gmail.com>
2024-02-16 09:01:34 +0700

    final

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -7,17 +7,24 @@ public class Main {
                 ***************************************************
                 Don't Give up this opportunity, do your best first
                 ---------------------------------------------------
-                Downloading.............Completed 100%.
-                """;
+                Downloading.............
+                """ ;
 
-
+        int i=0;
         for (char letter : text.toCharArray()) {
+
             System.out.print(letter);
             try {
-                Thread.sleep(200);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            if(i==text.length()-2){
+                System.out.print("Completed 100%.");
+            }
+            i++;
         }
+
+
     }
 }
